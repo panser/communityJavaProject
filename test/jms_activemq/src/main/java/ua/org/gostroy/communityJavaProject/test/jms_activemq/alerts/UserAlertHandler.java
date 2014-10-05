11653851847e4f3a950c82794ba1813b4edb1381
Sergey Panov <gostroy@gmail.com>
2014-10-05 20:15:32 +0300

    add not finished jms_activemq module

--- test/jms_activemq/src/main/java/ua/org/gostroy/communityJavaProject/test/jms_activemq/alerts/UserAlertHandler.java
+++ test/jms_activemq/src/main/java/ua/org/gostroy/communityJavaProject/test/jms_activemq/alerts/UserAlertHandler.java
@@ -7,23 +7,43 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.JmsUtils;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Panov Sergey on 10/1/2014.
  */
-public class UserAlertHandler {
+public class UserAlertHandler implements MessageListener {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
 
     @Autowired
     JmsTemplate jmsTemplate;
+    @Autowired
+    private AtomicInteger counter = null;
 
+/*
     public String processAlert(){
         try {
             ObjectMessage receivedMessage = (ObjectMessage) jmsTemplate.receive();
             String message =  (String) receivedMessage.getObject();
-            LOG.trace(getClass() + ": async recive message: " + message);
+            LOG.trace(getClass() + ": Async recive message: " + message);
+            counter.incrementAndGet();
             return message;
+        } catch (JMSException jmsException) {
+            throw JmsUtils.convertJmsAccessException(jmsException);
+        }
+    }
+*/
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            ObjectMessage receivedMessage = (ObjectMessage) jmsTemplate.receive();
+            String messageReceive =  (String) receivedMessage.getObject();
+            LOG.trace(getClass() + ": Async recive message: " + messageReceive);
+            counter.incrementAndGet();
         } catch (JMSException jmsException) {
             throw JmsUtils.convertJmsAccessException(jmsException);
         }
--- test/jms_activemq/src/test/java/ua/org/gostroy/communityJavaProject/test/jms_activemq/service/AlertServiceTest.java
+++ test/jms_activemq/src/test/java/ua/org/gostroy/communityJavaProject/test/jms_activemq/service/AlertServiceTest.java
@@ -9,7 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import ua.org.gostroy.communityJavaProject.core_entity.entity.User;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Panov Sergey on 10/1/2014.
@@ -18,10 +19,11 @@ import ua.org.gostroy.communityJavaProject.core_entity.entity.User;
 @ContextConfiguration("classpath:/ua/org/gostroy/communityJavaProject/test/jms_activemq/applicationContext.xml")
 public class AlertServiceTest {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
-    User testUser;
 
     @Autowired
     AlertService alertService;
+    @Autowired
+    private AtomicInteger counter = null;
 
     @Test
     @Ignore
@@ -36,12 +38,17 @@ public class AlertServiceTest {
 
     @Test
     @Ignore
-    public void asyncJmsTest(){
+    public void asyncJmsTest() throws Exception{
         LOG.trace(getClass() + ": asyncJmsTest() ...");
         String sendMessage = "testAJms";
-        alertService.sendAlert(sendMessage);
-        String receiveMessage = alertService.getAlert();
-        Assert.assertEquals(sendMessage, receiveMessage);
+        for (int i = 0; i< 100; i++){
+            alertService.sendAlert(sendMessage);
+        }
+
+        // give listener a chance to process messages
+        Thread.sleep(2 * 1000);
+
+        LOG.trace(getClass() + ": asyncJmsTest(). Receive messages count = " + counter);
         LOG.trace(getClass() + ": asyncJmsTest().");
     }
 
