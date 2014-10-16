dac4b81f37c2e6b1f5ec53adf32c6eaa5c31e80d
Sergey Panov <gostroy@gmail.com>
2014-10-16 20:51:45 +0300

    change maven modules structure

--- web/datagrid_easyui/src/main/java/ua/org/gostroy/communityJavaProject/web/datagrid_easyui/controller/excel/ExcelBuilder.java
+++ web/datagrid_easyui/src/main/java/ua/org/gostroy/communityJavaProject/web/datagrid_easyui/controller/excel/ExcelBuilder.java
@@ -1,9 +1,6 @@
 package ua.org.gostroy.communityJavaProject.web.datagrid_easyui.controller.excel;
 
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
@@ -11,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
 import ua.org.gostroy.communityJavaProject.core_entity.entity.User;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -29,13 +27,15 @@ public class ExcelBuilder extends AbstractExcelView{
         sheet.setDefaultColumnWidth(30);
 
         // create style for header cells
-        CellStyle style = workbook.createCellStyle();
         Font font = workbook.createFont();
         font.setFontName("Arial");
-        style.setFillForegroundColor(HSSFColor.BLUE.index);
-        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
         font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         font.setColor(HSSFColor.WHITE.index);
+
+        CellStyle style = workbook.createCellStyle();
+        style.setDataFormat(HSSFDataFormat.getBuiltinFormat("m/d/yy h:mm"));
+        style.setFillForegroundColor(HSSFColor.BLUE.index);
+        style.setFillPattern(CellStyle.SOLID_FOREGROUND);
         style.setFont(font);
 
         // create header row
@@ -63,6 +63,7 @@ public class ExcelBuilder extends AbstractExcelView{
             aRow.createCell(2).setCellValue(user.getEmail());
             aRow.createCell(3).setCellValue(user.getPassword());
         }
-
     }
+
+
 }
